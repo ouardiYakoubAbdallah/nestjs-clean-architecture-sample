8c02ef7c32fef5293b867f18519eaf97caa48660
Yakoub Ouardi <ouardi.yakoubabdallah@gmail.com>
2022-10-24 13:41:05 +0100

    feat: add the different modules to the app ✨

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 import { TypeOrmConfigModule } from './infrastructure/config/typeorm/typeorm.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
+import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
+import { UsecasesProxyModule } from './infrastructure/usecases-proxy/usecases-proxy.module';
+import { ControllersModule } from './infrastructure/controllers/controllers.module';
 
 @Module({
-    imports: [EnvironmentConfigModule, TypeOrmConfigModule, LoggerModule, ExceptionsModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        EnvironmentConfigModule,
+        TypeOrmConfigModule,
+        LoggerModule,
+        ExceptionsModule,
+        RepositoriesModule,
+        UsecasesProxyModule,
+        ControllersModule,
+    ],
 })
 export class AppModule {}

6dc89302b699e6e0586272d52a59ce3243f0ca4b
Yakoub Ouardi <ouardi.yakoubabdallah@gmail.com>
2022-10-24 13:41:53 +0100

    feat: register filters and interceptors ✨

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './infrastructure/common/filter/exception.filter';
+import { LoggingInterceptor } from './infrastructure/common/interceptors/logger.interceptor';
+import { ResponseInterceptor } from './infrastructure/common/interceptors/response.interceptor';
+import { LoggerService } from './infrastructure/logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+    app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+    app.useGlobalInterceptors(new ResponseInterceptor());
+    await app.listen(3000);
 }
 bootstrap();
